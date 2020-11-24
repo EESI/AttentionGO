47b42f090b8896c2b07b17b8bc2aa9a6aa66d245
Zhengqiao Zhao <zz374@picotte001.cm.cluster>
2020-11-24 18:56:57 -0500

    added mount

---
+++ mount/__init__.py
@@ -0,0 +1,8 @@
+import mount
+import mount.train_nn_mt
+import mount.train_nn_st
+import mount.train_nn_mt_ko
+from mount.nn_modules import SingleModel, CrossStitchModel, CrossStitchModel_KO
+
+# from mount.utils import Data
+# from mount.nn_modules import SingleModel, CrossStitchModel
